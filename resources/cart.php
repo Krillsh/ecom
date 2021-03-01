d54fcfac4d3ebc303217a41ff6ec3a2904300297
Kristoffer Calapis <kristoffercalapis@hotmail.com>
2021-03-01 21:12:01 +0100

    fixed search ways

--- resources/cart.php
+++ resources/cart.php
@@ -93,9 +93,9 @@ function cart(){
             <td>&#36;{$row['product_price']}</td>
             <td>{$value}</td>
             <td>&#36;{$sub}</td>
-            <td><a class='btn btn-warning' href="cart.php?remove={$row['product_id']}"><span class='glyphicon glyphicon-minus'></span></a>      
-            <a class='btn btn-success' href="cart.php?add={$row['product_id']}"><span class='glyphicon glyphicon-plus'</span></a>     
-            <a class='btn btn-danger' href="cart.php?delete={$row['product_id']}"><span class='glyphicon glyphicon-remove'</span></a>     
+            <td><a class='btn btn-warning' href="../resources/cart.php?remove={$row['product_id']}"><span class='glyphicon glyphicon-minus'></span></a>      
+            <a class='btn btn-success' href="../resources/cart.php?add={$row['product_id']}"><span class='glyphicon glyphicon-plus'</span></a>     
+            <a class='btn btn-danger' href="../resources/cart.php?delete={$row['product_id']}"><span class='glyphicon glyphicon-remove'</span></a>     
             </tr>
 
             <input type="hidden" name="item_name_{$item_name}" value="{$row['product_title']}">
