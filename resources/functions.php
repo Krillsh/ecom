46eaee2f43c2bec4494d4792a3c3463eefd1be19
Kristoffer Calapis <kristoffercalapis@hotmail.com>
2021-03-16 00:14:04 +0100

    upgrades

--- resources/functions.php
+++ resources/functions.php
@@ -481,4 +481,83 @@ set_message("Category Created");
 
 }
 }
+
+
+/************* Admin users   **************/
+
+function display_users(){
+
+    $category_query = query("SELECT * FROM users");
+    confirm($category_query);
+    
+    while($row = fetch_array($category_query)) {
+    
+    $user_id = $row['user_id'];
+    $username = $row['username'];
+    $email = $row['email'];
+    $password = $row['password'];
+    
+    $users = <<<DELIMITER
+    
+    <tr>
+        <td>{$user_id}</td>
+        <td>{$username}</td>
+        <td>{$email}</td>
+        <td><a class="btn btn-danger" href="../../resources/templates/back/delete_user.php?id={$row['user_id']}"><span class="glyphicon glyphicon-remove" ></span></a></td>
+    </tr>
+    
+    DELIMITER;
+    
+    echo $users;
+    }
+    }
+
+    function add_user() {
+
+        if(isset($_POST['add_user'])) {
+
+            $username = escape_string($_POST['username']);
+            $email = escape_string($_POST['email']);
+            $password = escape_string($_POST['password']);
+            //$user_photo = $_FILES['file']['name'];
+            //$photo_temp = $FILES['file']['tmp_name'];
+
+            move_uploaded_file($photo_temp, UPLOAD_DIRECTORY . DS . $user_photo);
+
+            $query = query("INSERT INTO users(username, email, password) VALUES('{$username}', '{$email}', '{$password}')");
+            confirm($query);
+
+            set_message("User Created");
+            redirect("index.php?users");
+        }
+
+    }
+
+    function get_reports(){
+
+        $query = query("SELECT * FROM reports");
+        confirm($query);
+            
+        while($row = fetch_array($query)) {
+        
+        $report = <<<DELIMETER
+            <tr>
+                <td>{$row['report_id']}</td>
+                <td>{$row['product_id']}</td>
+                <td>{$row['order_id']}</td>
+                <td>{$row['product_price']}</td>
+                <td>{$row['product_title']} </td>
+                <td>{$row['product_quantity']}</td>
+                <td><a class="btn btn-danger" href="../../resources/templates/back/delete_product.php?id={$row['product_id']}"><span class="glyphicon glyphicon-remove" ></span></a></td>
+            </tr>
+        
+        DELIMETER;
+        
+        echo $report;   
+            
+        }
+        
+        }
+        
+
 ?>
